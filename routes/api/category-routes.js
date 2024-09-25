4022daeefe23458752b5bef70b56ba8e291e4d20
yinwu173 <yin.wu173@gmail.com>
2024-09-24 20:53:02 -0700

    created a GET route for category id and included its associated products + created a POST route for creating a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,17 +15,45 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (error) {
-    res.status(500).json({error: 'Failed to retrieve categories'});
+    res.status(500).json({ error: 'Failed to retrieve categories' });
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({
+      where: { id: req.params.id },
+
+      include: [{
+        model: Product,
+        through: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No location found with this id.' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve a category' });
+  }
 });
 
-router.post('/', (req, res) => {
+
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json({ message: 'Error creating a new category.' })
+  }
 });
 
 router.put('/:id', (req, res) => {
