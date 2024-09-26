ce99a397d2b52ad9dca93b20f981bac6dfe37713
yinwu173 <yin.wu173@gmail.com>
2024-09-25 18:55:59 -0700

    added DELETE route for deleting one product by its id for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -134,8 +134,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id.' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(400).json({ message: 'Error deleting the product.' })
+  }
 });
 
 module.exports = router;
